f12cadb2a1848428908a3777d5448a5bced26622
Serendipity <709517055@qq.com>
2022-04-24 23:47:18 +0800

    Week08 作业2、6

---
+++ week0802/src/main/java/com/example/week82/Week82Application.java
@@ -0,0 +1,13 @@
+package com.example.week82;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week82Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week82Application.class, args);
+    }
+
+}
